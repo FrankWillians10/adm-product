ab92c8a783c4bee50a2d60a6b9ccd1922e417429
Frank Willians <frankkwillians@gmail.com>
2025-07-02 05:05:10 -0400

    Adicionando novos testes no usecase de criacao de product

--- application/src/test/java/com/adm/product/application/product/create/CreateProductUseCaseTest.java
+++ application/src/test/java/com/adm/product/application/product/create/CreateProductUseCaseTest.java
@@ -1,18 +1,32 @@
 package com.adm.product.application.product.create;
 
 
+import com.adm.product.domain.exceptions.DomainException;
 import com.adm.product.domain.product.Product;
 import com.adm.product.domain.product.ProductGateway;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Objects;
 
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
-
+@ExtendWith(MockitoExtension.class)
 public class CreateProductUseCaseTest {
+
+    @InjectMocks
+    private DefaultCreateProductUseCase useCase;
+
+    @Mock
+    private ProductGateway productGateway;
 
     // Teste do caminho feliz
     // Teste passando uma propriedade inválida
@@ -28,18 +42,15 @@ public class CreateProductUseCaseTest {
 
         final var aCommand = CreateProductCommand.with(expectedName, expectedBrand, expectedDescription, expectedPrice);
 
-        final ProductGateway productGateway = Mockito.mock(ProductGateway.class);
-        Mockito.when(productGateway.create(Mockito.any()))
+        when(productGateway.create(any()))
                 .thenAnswer(returnsFirstArg());
-
-        final var useCase = new DefaultCreateProductUseCase(productGateway);
 
         final var actualOutput = useCase.execute(aCommand);
 
         Assertions.assertNotNull(actualOutput);
         Assertions.assertNotNull(actualOutput.id());
 
-        Mockito.verify(productGateway, Mockito.times(1))
+        Mockito.verify(productGateway, times(1))
                 .create(Mockito.argThat(aProduct -> {
 
                     return Objects.equals(expectedName, aProduct.getName())
@@ -48,5 +59,56 @@ public class CreateProductUseCaseTest {
                             && Objects.equals(expectedPrice, aProduct.getPrice());
                 }
         ));
+    }
+
+    @Test
+    public void givenAInvalidName_whenCallsCreateProduct_thenShouldReturnDomainException() {
+        final String expectedName = null;
+        final var expectedBrand = "Apple";
+        final var expectedDescription = "Um lancamento Apple 2025";
+        final var expectedPrice = 10.000;
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedErrorCount = 1;
+
+        final var aCommand =
+                CreateProductCommand.with(expectedName, expectedBrand, expectedDescription, expectedPrice);
+
+        final var actualException =
+                Assertions.assertThrows(DomainException.class, () -> useCase.execute(aCommand));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getMessage());
+
+        Mockito.verify(productGateway, times(0)).create(any());
+    }
+
+    @Test
+    public void givenAValidCommand_whenGatewayThrowsRandomException_shouldReturnException() {
+
+        final var expectedName = "Iphone 13";
+        final var expectedBrand = "Apple";
+        final var expectedDescription = "Um lancamento Apple 2025";
+        final var expectedPrice = 10.000;
+        final var expectedErrorMessage = "Gateway error";
+
+        final var aCommand =
+                CreateProductCommand.with(expectedName, expectedBrand, expectedDescription, expectedPrice);
+
+        when(productGateway.create(any()))
+                .thenThrow(new IllegalStateException(expectedErrorMessage));
+
+        final var actualException =
+                Assertions.assertThrows(IllegalStateException.class, () -> useCase.execute(aCommand));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getMessage());
+
+        Mockito.verify(productGateway, times(1))
+                .create(Mockito.argThat(aProduct -> {
+
+                            return Objects.equals(expectedName, aProduct.getName())
+                                    && Objects.equals(expectedBrand, aProduct.getBrand())
+                                    && Objects.equals(expectedDescription, aProduct.getDescription())
+                                    && Objects.equals(expectedPrice, aProduct.getPrice());
+                        }
+                ));
     }
 }
