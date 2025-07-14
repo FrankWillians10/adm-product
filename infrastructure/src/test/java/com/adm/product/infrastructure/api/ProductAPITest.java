8e190e5a6a3bede1b545e58d3360510dc4b6eb67
Frank Willians <frankkwillians@gmail.com>
2025-07-13 23:02:51 -0400

    Adicionando testes na api

--- infrastructure/src/test/java/com/adm/product/infrastructure/api/ProductAPITest.java
+++ infrastructure/src/test/java/com/adm/product/infrastructure/api/ProductAPITest.java
@@ -5,7 +5,10 @@ import com.adm.product.application.Either;
 import com.adm.product.application.product.create.CreateProductCommand;
 import com.adm.product.application.product.create.CreateProductOutput;
 import com.adm.product.application.product.create.CreateProductUseCase;
+import com.adm.product.domain.exceptions.DomainException;
 import com.adm.product.domain.product.ProductID;
+import com.adm.product.domain.validation.Error;
+import com.adm.product.domain.validation.handler.Notification;
 import com.adm.product.infrastructure.product.models.CreateProductApiInput;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -20,6 +23,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Objects;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -58,10 +63,9 @@ public class ProductAPITest {
 
         this.mvc.perform(request)
                 .andDo(print())
-                .andExpectAll(
-                        status().isCreated(),
-                        header().string("Location", "/products/123")
-                );
+                .andExpectAll(status().isCreated())
+                .andExpectAll(header().string("Location", "/products/123"))
+                .andExpectAll(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE));
 
         verify(createProductUseCase, times(1)).execute(argThat(cmd ->
                         Objects.equals(expectedName, cmd.name())
@@ -69,6 +73,73 @@ public class ProductAPITest {
                         && Objects.equals(expectedDescription, cmd.description())
                         && Objects.equals(expectedPrice, cmd.price())
                 ));
+    }
+
+    @Test
+    public void givenAInvalidName_whenCallsCreateProduct_thenShouldReturnNotification() throws Exception {
+        final String expectedName = null;
+        final var expectedBrand = "Apple";
+        final var expectedDescription = "Um lancamento Apple 2025";
+        final var expectedPrice = 10.000;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var aInput = new CreateProductApiInput(expectedName, expectedBrand, expectedDescription, expectedPrice);
+
+        when(createProductUseCase.execute(any()))
+                .thenReturn(Either.left(Notification.create(new Error(expectedErrorMessage))));
+
+        final var request = post("/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(aInput));
+
+        this.mvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(header().string("Location",nullValue()))
+                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.errors", hasSize(1)))
+                .andExpect(jsonPath("$.errors[0].message", equalTo(expectedErrorMessage)));
+
+        verify(createProductUseCase, times(1)).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedBrand, cmd.brand())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedPrice, cmd.price())
+        ));
+    }
+
+    @Test
+    public void givenAInvalidCommand_whenCallsCreateProduct_thenShouldReturnDomainException() throws Exception {
+        final String expectedName = null;
+        final var expectedBrand = "Apple";
+        final var expectedDescription = "Um lancamento Apple 2025";
+        final var expectedPrice = 10.000;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var aInput = new CreateProductApiInput(expectedName, expectedBrand, expectedDescription, expectedPrice);
+
+        when(createProductUseCase.execute(any()))
+                .thenThrow(DomainException.with(new Error(expectedErrorMessage)));
+
+        final var request = post("/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(aInput));
+
+        this.mvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(header().string("Location",nullValue()))
+                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.message", equalTo(expectedErrorMessage)))
+                .andExpect(jsonPath("$.errors", hasSize(1)))
+                .andExpect(jsonPath("$.errors[0].message", equalTo(expectedErrorMessage)));
+
+        verify(createProductUseCase, times(1)).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedBrand, cmd.brand())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedPrice, cmd.price())
+        ));
     }
 
 }
